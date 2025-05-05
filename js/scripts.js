dc5caea8aadff6204bd4992ae487374a3c5918e9
hmarengo <marengohernan@gmail.com>
2025-05-05 13:02:18 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+document.getElementById("mapaToggle").addEventListener("click", function () {
+	const mapa = document.getElementById("mapa");
+	mapa.style.display = mapa.style.display === "block" ? "none" : "block";
+});
