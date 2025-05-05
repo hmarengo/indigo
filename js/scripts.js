f8241a7e9f397cf40d6178e897fb3ef06fb77490
hmarengo <marengohernan@gmail.com>
2025-05-05 14:08:58 -0300

    Delete js directory

--- js/scripts.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("mapaToggle").addEventListener("click", function () {
-	const mapa = document.getElementById("mapa");
-	mapa.style.display = mapa.style.display === "block" ? "none" : "block";
-});
