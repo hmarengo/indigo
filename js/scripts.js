46ccb3a35ccaa4243a8529fac0c7a024b16e77b1
hmarengo <marengohernan@gmail.com>
2025-05-28 15:44:33 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+document.getElementById("mapaToggle").addEventListener("click", function () {
+	const mapa = document.getElementById("mapa");
+	mapa.style.display = mapa.style.display === "block" ? "none" : "block";
+});
+
+/*
+var talleres = "TALLER 
+Trabajando con adultos mayores.
+Herramientas y recursos.
+Beneficios del taller:
+- Mejorar la salud físico y mental 
+- Promover la interacción social  y sentimiento de pertenencia
+- Mantener la mente activa y desarrollar  nuevas habilidades.
+TALLERES:
+* Estimulacion psicomotora: propone actividades para mejorar la movilidad, autonomía, disminuir el stres y mejorar el bienestar general.
+* Taller de la memoria: ayuda a mantener la mente activa y a mejorar la memoria a través de ejercicios y actividades."
+*/
